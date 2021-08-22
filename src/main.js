90e04f935a3c0da1933ca0ce97da0245ad2fb0ea
menno4000 <maxluedemann@t-online.de>
2021-08-22 22:33:41 +0200

    remove vue-plyr

--- src/main.js
+++ src/main.js
@@ -9,17 +9,12 @@ import Axios from 'axios'
 import VueCollapse from "vue2-collapse";
 import Vuetify from "vuetify";
 import 'vuetify/dist/vuetify.min.css';
-// import VuePlyr from 'vue-plyr'
-import 'vue-plyr/dist/vue-plyr.css'
 
 Vue.config.productionTip = false
 Vue.use(VueEllipseProgress);
 Vue.use(vueSmoothScroll)
 Vue.use(Vuetify)
 Vue.use(VueCollapse)
-// Vue.use(VuePlyr, {
-//   plyr:{}
-// })
 
 Axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.token}`;
 
